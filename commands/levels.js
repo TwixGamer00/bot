1ca02bce903a94aecef781d24e92d2da8d2aac62
TwixGamer00 <64733980+TwixGamer00@users.noreply.github.com>
2021-02-14 17:04:36 +0100

    Add files via upload

---
+++ commands/levels.js
@@ -0,0 +1,53 @@
+const { createCanvas, loadImage } = require('canvas');
+const { MessageAttachment } = require('discord.js');
+const canvacord = require("canvacord");
+// eslint-disable-next-line no-unused-vars
+module.exports.run = async (client, message, args, prefix, embed) => {
+    if (!args[0]) args[0] = message.author.id;
+	client.GetMemberFromArg(args[0], message.guild.members).then(async member => {
+		if (!member) member = message.member;
+		const level = await client.db.level.get(`${message.guild.id}/${member.id}`) || 0;
+		const xp = await client.db.xp.get(`${message.guild.id}/${member.id}`) || 0;
+		const oldXP = await client.xpNeeded(level);
+		const newXP = await client.xpNeeded(level + 1);
+		const avatarURL = await getProfilePic(member.user);
+
+		const rankCard = new canvacord.Rank()
+        .setAvatar(avatarURL)
+        .setCurrentXP(xp)
+        .setLevel(level)
+        .setRank(1, "rank", false)
+        .setStatus(member.presence.status)
+        .setRequiredXP(newXP)
+        .setProgressBar("GREEN", "COLOR")
+        .setUsername(member.user.username)
+        .setDiscriminator(member.user.discriminator);
+        rankCard.build()
+        .then(data => {
+            const image = new MessageAttachment(data, "RankCard.png");
+
+            message.channel.send(image);
+        });
+		
+	}).catch(err => {
+		return message.channel.send(err);
+	});
+};
+
+
+exports.help = {
+	name: 'levels',
+	category: 'Levels',
+	description: 'Check someone their level',
+	usage: 'levels [GuildMember]',
+};
+
+
+
+function getProfilePic(user) {
+	if (user.avatar == null) {
+		return `https://cdn.discordapp.com/embed/avatars/${parseInt(user.discriminator) % 5}.png`;
+	} else {
+		return `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+	}
+}
